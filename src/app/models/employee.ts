832970a5b071d67dbdcb613b13f932759f1d7cf0
Can Nguyen <nguyencan@google.com>
2022-06-12 01:18:48 -0700

    add src

---
+++ src/app/models/employee.ts
@@ -0,0 +1,12 @@
+
+
+export interface Employee {
+
+    id: number;
+    name: String;
+    email: String;
+    jobTitle: String;
+    phone: String;
+    imageUrl:String;
+    employeeCode:String;
+}
